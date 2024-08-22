cc7dcf3ff5acb86f94fcc1e2cd9e15966de41fea
JorgeBlanco <jorge.blanco@berkeley.edu>
2024-08-22 13:33:44 -0700

    news

--- newton.py
+++ newton.py
@@ -1,18 +1,36 @@
-import numpy as np 
+import numpy as np
+
+def der_1(f,x):
+    """
+    Takes: a function f and real value x, 
+    Returns: an approximation of the derivative of f at that point 
+    """
+    h = 0.001
+    return (f(x+h) - f(x))/h
+
+def der_2(f,x):
+    
+    """
+    Takes: a function f and real value x, 
+    Returns: an approximation of the second derivative of f at that point 
+    """
+    h = 0.001
+    return (f(x+h) -2*f(x) +f(x-h) )/h**2
+    
 
 def optimize(x_0,f):
-    def der_1(f,x):
-        h = 0.001
-        return (f(x+h) - f(x))/h
-    
-    def der_2(f,x):
-        h = 0.001
-        return (f(x+h) -2*f(x) +f(x-h) )/h**2
-    
+    """
+    Input:
+    - x_0 : initialization point 
+    - A function we want to optimize 
+    returns:
+    - an approximate minimizer. 
+    """
     error = 0.00001
-    x_t = x_0
-    y_t = x_t - der_1(f,x_t)/der_2(f,x_t)
-    while np.abs(x_t - y_t) > error:
-        x_t = y_t
-        y_t =  x_t - der_1(f,x_t)/der_2(f,x_t)
-    return y_t
+    x = x_0
+    y = x - der_1(f,x)/der_2(f,x)
+    while np.abs(x - y) > error:
+        x = y
+        y =  x - der_1(f,x)/der_2(f,x)
+    return y
+
