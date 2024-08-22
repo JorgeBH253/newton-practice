5f191abb663dadb41d2ac562edbd9b7d76fa21f7
JorgeBH253 <jorge.blanco@berkeley.edu>
2024-08-22 13:00:44 -0700

    Update newton.py
    Thanks lucas !

--- newton.py
+++ newton.py
@@ -1,18 +1,20 @@
-import numpy as np 
+import numpy as np
 
-def optimize(x_0,f):
-    def der_1(f,x):
+def der_1(f,x):
         h = 0.001
         return (f(x+h) - f(x))/h
-    
-    def der_2(f,x):
+
+def der_2(f,x):
         h = 0.001
         return (f(x+h) -2*f(x) +f(x-h) )/h**2
     
+
+def optimize(x_0,f):
     error = 0.00001
-    x_t = x_0
-    y_t = x_t - der_1(f,x_t)/der_2(f,x_t)
-    while np.abs(x_t - y_t) > error:
-        x_t = y_t
-        y_t =  x_t - der_1(f,x_t)/der_2(f,x_t)
+    x = x_0
+    y = x - der_1(f,x)/der_2(f,x)
+    while np.abs(x - y) > error:
+        x = y
+        y =  x - der_1(f,x)/der_2(f,x)
     return y_t
+
